599fb6f88ae74f2f066a9352ce82ea07399f5a2c
derfrodo <derfrodo0715@gmail.com>
2017-03-12 16:17:39 +0100

    Nach rumprobiererei!

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ Mehr zum Framework findest Du unter: http://expressjs.com/
 
 var express = require("express");
 
+//Deklariere und Initielisiere express-App
 var app = express();
+var http = require('http').createServer(app);
+
+
+// app.get("/", (req, res)=>{
+//     res.send("Hallo Viewer aus server.js");
+// })
 
 app.use(express.static('www'));
-app.use("/lib", express.static('node_modules/p5'));
-app.use("/lib", express.static('node_modules/p5/addons'));
+app.use("/lib", express.static('node_modules/p5/lib'));
+app.use("/lib", express.static('node_modules/p5/lib/addons'));
+app.use("/lib", express.static('node_modules/socket.io-client/dist'));
 
-app.listen(3000, function(){
-    console.log("Express app is listening on port 3000. Visit it at http://localhost/3000 ");
+let paths = {};
+
+var io = require('socket.io')(http);
+io.on('connection', function (client) {
+    client.on('event', function (data) {
+        console.log(data);
+    });
+    client.on('disconnect', () => {
+        console.log("disconnect");
+
+        let clientId = client.id;
+        if (paths[clientId]) {
+            delete paths[clientId];
+            emitPaths(paths);
+        }
+    });
+    client.on("update path", (data) => {
+        console.log("update path");
+        let clientId = client.id;
+        paths[clientId] = paths[clientId] || {};
+        paths[clientId].color = data.path.color;
+        paths[clientId].clientPaths = data.path.paths;
+        paths[clientId].currentClientPath= data.path.currentPath;
+        
+        paths[clientId].currentClientPath = data.path.currentPath;
+        emitPaths(paths, client.broadcast);
+        console.log(paths[clientId]);
+    });
+
+    console.log("connect");
+    console.log(client.id);
+    //io.sockets.emit('new client', client.id);
+    client.broadcast.emit('new client', client.id);
+
 });
+
+function emitPaths(paths, emitter) {
+    console.log("Emit paths:");
+    console.log(paths);
+    emitter = emitter || io;
+    emitter.emit("paths updated",
+        { paths });
+}
+
+http.listen(3000, "0.0.0.0", () => {
+    console.log("Express app is listening on port 3000. Visit it at http://localhost:3000 ");
+});
+
+// app.listen(3000, function(){
+//    
+// });
--- www/js/sketch.js
+++ www/js/sketch.js
@@ -1,7 +1,113 @@
+var socket;
 function setup() {
-    
+
+    createP("Hallo Viewer!");
+    createCanvas(1280, 720);
+
+    socket = io.connect();
+    socket.on("new client", (data) => {
+        // console.log("new client");
+        // console.log(data);
+    })
+
+    socket.on("paths updated", (data) => {
+        paths = data.paths;
+        // console.log(data);
+    })
+}
+var paths;
+
+var path = {
+    color: {
+        r: Math.floor(Math.random() * 256),
+        b: Math.floor(Math.random() * 256),
+        g: Math.floor(Math.random() * 256)
+    },
+    paths: [],
+    currentPath: undefined,
+};
+
+function mousePressed() {
+
+    if (path.currentPath) {
+        path.paths.push(path.currentPath);
+    }
+
+    path.currentPath = [];
+    path.currentPath.push({
+        x: mouseX,
+        y: mouseY
+    });
+    // console.log("send new path")
+    emitUpdatedPaths();
 }
 
+function mouseDragged() {
+    path.currentPath.push({
+        x: mouseX,
+        y: mouseY
+    });
+    emitUpdatedPaths();
+    // console.log("update path")
+}
+
+function emitUpdatedPaths() {
+    if (!updateHandle) {
+        updateHandle = setTimeout(() => {
+            socket.emit("update path", { path });
+            updateHandle = undefined;
+        }, 500);
+    }
+}
+
+var updateHandle = undefined;
+
 function draw() {
-    
+
+    background(255);
+    noFill();
+    stroke(0);
+    strokeWeight(1);
+    rect(0, 0, width - 1, height - 1);
+
+    if (paths) {
+        for (let cpDataId in paths) {
+            if (cpDataId !== socket.id) {
+                clientPathData = paths[cpDataId];
+                for (let p of clientPathData.clientPaths) {
+                    drawPath(p, clientPathData.color);
+                }
+
+                if (clientPathData.currentClientPath) {
+                    drawPath(clientPathData.currentClientPath, clientPathData.color);
+                }
+            }
+        }
+    }
+
+    if (path) {
+        for (let p of path.paths) {
+            drawPath(p, path.color);
+        }
+        if(path.currentPath){
+
+        drawPath(path.currentPath, { r: 255, g: 0, b: 255 });
+        }
+    }
+
+}
+
+function drawPath(path, cpColor) {
+    push();
+    let drawColor = color(cpColor.r, cpColor.g, cpColor.b);
+
+    strokeWeight(4);
+    stroke(drawColor);
+    beginShape();
+    for (let i = 0; i < path.length; i++) {
+        vertex(path[i].x, path[i].y)
+    }
+    endShape();
+
+    pop();
 }
